a74cbf9a28706bb59ad6ff8c7db2ed335cde7bb4
Gabriel Muniz <56240854+Gabriel-Muniz@users.noreply.github.com>
2023-09-26 20:45:55 -0300

    add drawing to the grid

--- main.js
+++ main.js
@@ -4,13 +4,15 @@ let totalCells = 256;
 for (let i = 0; i < totalCells; i++) {
     let div = document.createElement('div');
     div.classList.add('grid')
-    div.setAttribute('id', i+1)
+    div.setAttribute('id', i + 1)
     divArray[i] = div;
-    console.log(divArray[i])
+    container.appendChild(div)
 }
 
+container.style.width = `${Math.sqrt(totalCells) * 50}px`;
+console.log(divArray.length)
 divArray.forEach(gridIndividual => {
-    container.appendChild(gridIndividual)
+    gridIndividual.addEventListener('mouseover', () => {
+        gridIndividual.style.backgroundColor = 'black';
+    })
 });
-
-container.style.width = `${Math.sqrt(totalCells)*50}px`;
